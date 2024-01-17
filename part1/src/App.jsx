4dd0803ef6175b9062ea83b18e2edafa595df7ee
chuchoramirez195@gmail.com <chuchoramirez195@gmail.com>
2024-01-17 11:51:14 -0600

    start commit

---
+++ part1/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Hello = (props) => {
+
+  console.log(props)
+  return (
+    <div>
+      <p>
+
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+
+  const name = 'PEPON'
+  const age = 30
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+
+      <Hello name='JOTA PE' age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+
+export default App
