daf4fce0a8f407385db3d76aed27f26f49cbfd0b
chuchoramirez195@gmail.com <chuchoramirez195@gmail.com>
2024-01-18 18:08:03 -0600

    Excersies 1.3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -12,27 +12,33 @@ const Content = ... {
 */
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+ const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <h1>{course}</h1>
       <p>
-        {part1} {exercises1}
+        {part1.exercises}
       </p>
       <p>
-        {part2} {exercises2}
+        {part2.exercises}
       </p>
       <p>
-        {part3} {exercises3}
+        {part3.exercises}
       </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
     </div>
   )
 }
