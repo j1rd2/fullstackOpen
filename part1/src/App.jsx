6f2b31d16ff0fc60dcb7caa6263d2a1c6efa138d
chuchoramirez195@gmail.com <chuchoramirez195@gmail.com>
2024-01-24 23:06:36 -0600

    Excercise 1.7

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -9,6 +9,23 @@ const App = () => {
   const sumGood = () => setGood(good + 1);
   const sumNeutral = () => setNeutral(neutral + 1);
   const sumBad = () => setBad(bad + 1);
+  const all = () => (good + neutral + bad);
+  const average = () => {
+
+    const total = good + neutral + bad;
+
+    if (total === 0){
+      return 0;
+    } else {
+      return ((good * 1) + (neutral * 0) + (bad * -1) / total)
+    }
+  }
+  const positive = () => {
+
+    const total = good + neutral + bad;
+
+    return (good * 100) / total;
+  }
 
   return (
     <div>
@@ -25,15 +42,13 @@ const App = () => {
         Bad
       </button>
       <h1>Statistics</h1>
-      <p>
-        {'Good' + good}
-      </p>
-      <p>
-        {'Neutral' + neutral}
-      </p>
-      <p>
-        {'Bad' + bad}
-      </p>
+      <p>{'Good' + good}</p>
+      <p>{'Neutral' + neutral}</p>
+      <p>{'Bad' + bad}</p>
+      <p>{'All: ' + all()}</p>
+      <p>{'average: ' + average()}</p>
+      <p>{'positive: ' + positive()}</p>
+
     </div>
   )
 }
