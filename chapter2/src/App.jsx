76a4dc11c54a40b22f6afd9eab7f3c6e489e6ec2
chuchoramirez195@gmail.com <chuchoramirez195@gmail.com>
2024-01-30 19:54:13 -0600

    Excercise 2.1: Course information step7

--- chapter2/src/App.jsx
+++ chapter2/src/App.jsx
@@ -1,99 +1,36 @@
-import { useState } from 'react'
-
-const Button = ({handleClick, text}) => {
-  return (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
-}
-
-const StatisticsLine = ({ text, value }) => {
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  );
-};
-
-const Statistics = ({ good, neutral, bad, all, average, positive }) => {
-  if (all === 0) {
-    return <p>No feedback given</p>;
+const Course = () =>  {
+  const course = {
+    name: 'Half Stack Application Developer',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of component',
+        exercises: 14
+      }
+    ]
   }
-
   return (
     <div>
-      <h1>Statistics</h1>
-      <table>
-        <tbody>
-          <StatisticsLine text="Good" value={good} />
-          <StatisticsLine text="Neutral" value={neutral} />
-          <StatisticsLine text="Bad" value={bad} />
-          <StatisticsLine text="All" value={all} />
-          <StatisticsLine text="Average" value={average} />
-          <StatisticsLine text="Positive" value={positive + '%'} />
-        </tbody>
-      </table>
+      <h1>{course.name}</h1>
+      <p>{course.parts[0].exercises}</p>
+      <p>{course.parts[1].exercises}</p>
+      <p>{course.parts[2].exercises}</p>
+      <p>
+        Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
+      </p>
     </div>
   );
-};
-
-
-const App = () => {
-
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const sumGood = () => setGood(good + 1);
-  const sumNeutral = () => setNeutral(neutral + 1);
-  const sumBad = () => setBad(bad + 1);
-  const all = () => (good + neutral + bad);
-  const average = () => {
-
-    const total = good + neutral + bad;
-
-    if (total === 0){
-      return 0;
-    } else {
-      return ((good * 1) + (neutral * 0) + (bad * -1) / total)
-    }
-  }
-  const positive = () => {
-
-    const total = good + neutral + bad;
-
-    return (good * 100) / total;
-  }
-
-  const allCount = all();
-  const averageScore = average();
-  const positivePercentage = positive();
-
-  return (
-    <div>
-
-      <h1>Give feedback</h1>
-
-    <Button handleClick={sumGood} text={'Good'}/>
-    <Button handleClick={sumNeutral} text={'Neutral'}/>
-    <Button handleClick={sumBad} text={'Bad'}/>
-
-
-
-      <h1>Statistics</h1>
-      <Statistics
-        good = {good}
-        neutral={neutral}
-        bad={bad}
-        all={allCount}
-        average={averageScore}
-        positive={positivePercentage}
-      />
-
-    </div>
-  )
 }
 
-export default App
+const App = () => (
+  <Course />
+)
+
+export default App;
